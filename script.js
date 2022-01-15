fc8fbe16dd7b4c475d222faa0cca1036cc8f7374
stephanie-alexandre <gomesstephanie976@gmail.com>
2022-01-15 14:26:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let name="Ste";
+let number="21";
+let subtract= 2022 - number;
+
+
+console.log (subtract + 21);
+console.log ("estou mexendo no console.log");
+
+
+
+let quartaFeira= false;
+console.log ({quartaFeira});
+console.log (typeof quartaFeira);
